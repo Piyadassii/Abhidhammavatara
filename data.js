0272c523d4d7efdef24a1dc19b0f4ecc0c9a63b5
Piya <piya@PiyadeMacBook-Air.local>
2020-09-30 08:54:32 +0630

    Initial upload

---
+++ data.js
@@ -0,0 +1,28 @@
+const chapters = ["chapter1", "chapter2", "chapter3"]
+
+const textJson = [{
+    "pali": "1. anantakaruṇāpaññaṃ tathāgatam anuttaraṃ <br> vanditvā sirasā buddhaṃ, dhammaṃ sādhugaṇam ’pi ca", 
+    "lit": "1. Having respectfully paid homage to the unsurpassed Tathāgata of infinite compassion and wisdom, the Buddha, the Dhamma and the excellent group [the Saṅgha],", 
+    "free":"1. To Tathāgata of boundless compassion and wisdom, <br>     To the Buddha, the Dhamma and the excellent group, <br>     I pay homage respectfully bowing my head.",
+    "composition":"verse"
+ }, {
+    "pali": "2. paṇḍukambalanāmāya, silāyātulavikkamo <br> nisinno devarājassa vimale sītale tale <br> 3. yaṃ devadevo devānaṃ, devadevehi pūjito <br> desesi devalokasmiṃ, dhammaṃ devapurakkhato", 
+    "lit": "2-3. the Dhamma that honoured by the deities, in front of the deities, the God of gods unequalled in power, seated on the spotless cool stone of the king of the deities, called paṇḍukambala, taught the deities in their world, ", 
+    "free":"2-3. I will explain in brief the Dhamma <br>    That God of gods in front of the gods <br>    Seated on the cool thronе of the king of gods <br>    Taught the Gods in the world of gods.",
+    "composition":"verse"
+ }, {
+    "pali": "tasmā anavajjaiṭṭhavipāka-lakkhaṇaṃ kusalaṃ, akusalaviddhaṃsana-rasaṃ, vodāna-paccupaṭṭhanaṃ; sāvajjapaṭipakkhattā anavajjam eva lakkhaṇaṃ  vā kusalaṃ, vodānabhāva-rasaṃ, iṭṭhavipāka-paccupaṭṭhānaṃ, yonisomanasikāra-padaṭṭhānaṃ. sāvajjāniṭṭhavipāka-lakkhaṇaṃ akusalaṃ; tadubhayaviparīta-lakkhaṇaṃ abyākataṃ, avipākārahaṃ vā.", 
+    "lit": "Therefore, the characteristic of the wholesome is faultlessness, and [ability to] produce agreeable results; its function is destruction of unwholesome;  its manifestation is purification. Or, its characteristic is faultlessness because it opposes the faulty; its function is [bringing about] the state of purity; its manifestation is production of agreeable results, and its proximate cause is proper attention. The characteristic of the unwholesome is faultiness and production of undesirable results; the indeterminate has its characteristic being opposite of both these two or not productive of results.", 
+    "free":"",
+    "composition":"prose"
+ }, {
+    "pali": "tattha cittan ’ti visayavijānanaṃ cittaṃ, tassa pana ko vacanattho?",
+    "lit": "Here, citta means consciousness as cognition of an object. What does it mean?",
+    "free": "Herein, 'consciousness' means consciousness as cognizing an object. What does it mean?",
+    "composition": "prose"},
+{
+    "pali": "pali5", 
+    "lit": "lit5", 
+    "free":"free5",
+    "composition":"verse"
+    }]

db9ec35056031dbf82551d49f721dbe43c85c619
Piya <piya@PiyadeMacBook-Air.local>
2020-10-19 10:00:21 +0630

    Chapter 1 Collation
    Added notebook with collation, deleted the web experiments

--- app.js
+++
@@ -1,9 +0,0 @@
-
-for ( let i=0; i<textJson.length; i++ ) {
-    let textKind = `<p class= "${textJson[i]['composition']}"`,
-    textPali = `<p class="pali"> ${textJson[i]['pali']}</p>`,
-    textLit = `<p class="lit"> ${textJson[i]['lit']}</p>`,
-    textFree = `<p class="free"> ${textJson[i]['free']}</p>`;
-    console.log(".pali")
-$(".container").append(textPali + textLit + textFree)
-}
